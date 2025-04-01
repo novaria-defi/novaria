2123505ad8aab0a948178ca966669bddb78f6606
Afrijal Dzuhri <hi@afrijal.dev>
2025-04-01 20:55:13 +0700

    update Arbitrum Sepolia configuration and add new token addresses

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,31 +1,28 @@
 import { createConfig, http } from "wagmi"
 import { type Chain } from "viem"
-import { mainnet, sepolia } from "viem/chains"
 
-export const rise = {
-  id: 11155931,
-  name: "Rise Sepolia",
+export const arbitrumSepolia = {
+  id: 421614,
+  name: "Arbitrum Sepolia",
   nativeCurrency: {
     name: "Ether",
     symbol: "ETH",
     decimals: 18,
   },
   rpcUrls: {
-    default: { http: ["https://testnet.riselabs.xyz"] },
+    default: { http: ["https://sepolia-rollup.arbitrum.io/rpc"] },
   },
   blockExplorers: {
     default: {
-      name: "Etherscan",
-      url: "https://testnet-explorer.riselabs.xyz",
+      name: "Arbiscan",
+      url: "https://sepolia.arbiscan.io",
     },
   },
 } as const satisfies Chain
 
 export const config = createConfig({
-  chains: [rise, mainnet, sepolia],
+  chains: [arbitrumSepolia],
   transports: {
-    [rise.id]: http(),
-    [mainnet.id]: http("https://mainnet.example.com"),
-    [sepolia.id]: http("https://sepolia.example.com"),
+    [arbitrumSepolia.id]: http(),
   },
 })
